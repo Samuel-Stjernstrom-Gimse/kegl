883209b6bb4fdb16a384daa6358d835bc82a48f2
Samuel Stjernstrøm-Gimse <153206324+Samuel-Stjernstrom-Gimse@users.noreply.github.com>
2024-11-07 19:33:57 +0100

    Update examples.js

--- examples.js
+++ examples.js
@@ -1,42 +1,49 @@
 import Component from "./framework/component.js";
 
 class MyComponent2 extends Component {
+    constructor() {
+        super();
+    }
     render() {
-        this.update(`<div id="div2">${this.state.value.params[1]}</div>`)
-
+        this.update(`<div id="div2">${this.params.value[0]}</div>`)
+        console.log(this.params.value);
         this.shadowRoot.querySelector("#div2").addEventListener("click", () => console.log('hello'));
     }
 }
-customElements.define('my-component2', MyComponent2);
+customElements.define('my-component1', MyComponent2);
 
 class MyComponent extends Component {
     render() {
+        const makeHtml = () => {
+            return [1, 2, 3, 5, 6, 7, 8].map((e,i) => {
+                return `<my-component1 params='${e}'></my-component1>`
+            }).join('')
+        }
+
         this.update(`
-            <div id="div2">
-                ${this.state.value.params}
+            <div>
+            <button id="div2">click me state :${this.subState.value} </button>
+            ${this.params.value}
             </div> 
-            <my-component2 params='[69, 2]'>
+            ${makeHtml()}
         `)
 
-        this.shadowRoot.querySelector("#div2").addEventListener("click", () => console.log('hello'));
+        const inputElement = document.getElementById("input")
+
+        const handleState = () => {
+            this.params.value = 'hey'
+            this.subState.value = this.subState.value + 1;
+        }
+
+        const handleInput = () => {
+            this.subState.value = inputElement.value
+        }
+
+        inputElement.addEventListener('change', () => {
+            handleInput()
+        });
+
+        this.attachEvent('#div2', 'click', () => handleState() );
     }
 }
-customElements.define('my-component', MyComponent)
-
-/*
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body style="margin: 0; padding: 0">
-
-<my-component params='100'></my-component>
-
-<script type="module" src="./examples.js"></script>
-</body>
-</html>
-
-*/
+customElements.define('my-component', MyComponent);
